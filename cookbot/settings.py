89ac9894caaa5a689a8b02310b89ceab7385432a
Naoki Orii <mrorii@gmail.com>
2013-07-09 22:23:52 -0700

    Initial commit

---
+++ cookbot/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for cookbot project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'cookbot'
+
+SPIDER_MODULES = ['cookbot.spiders']
+NEWSPIDER_MODULE = 'cookbot.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cookbot (+http://www.yourdomain.com)'
